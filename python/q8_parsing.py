eab3f28dc8c605a42415fe9ea336f309a9637543
Daniel Friedman <dfriedman33@gmail.com>
2017-03-19 18:12:12 -0700

    answer to question regarding parsing csv of English Premier League football team data

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,6 +1,20 @@
-# The football.csv file contains the results from the English Premier League. 
+import csv
+
+# The football.csv file contains the results from the English Premier League.
 # The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
 # goals scored for and against each team in that season (so Arsenal scored 79 goals 
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+# probably best to create a new dictionary with the name of the team and that difference value
+# then you can find the max in that dictionary and simply print the name of the key
+
+with open('football.csv') as csv_file:
+    football_dict = {}
+    reader = csv.reader(csv_file)
+    next(reader)  # skips the header row in football.csv
+    for row in reader:
+        absolute_goals_difference = abs(int(row[5]) - int(row[6]))
+        football_dict[row[0]] = absolute_goals_difference
+    team_smallest_goal_difference = min(football_dict, key=football_dict.get)
+    print(team_smallest_goal_difference)
