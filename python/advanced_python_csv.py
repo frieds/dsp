bfdc964d96e9adc472ef1437f6e38fdae4752642
Daniel Friedman <dfriedman33@gmail.com>
2017-03-21 19:24:27 -0700

    write emails to new csv file called emails.csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+import csv
+
+faculty_email_addresses = []
+with open('faculty.csv') as csv_file:
+    reader = csv.reader(csv_file)
+    next(reader)
+    for faculty in reader:
+        email_address = faculty[3]
+        faculty_email_addresses.append(email_address)
+
+with open('emails.csv', 'w') as csv_write_file:
+    write_emails = csv.writer(csv_write_file)
+    for email in faculty_email_addresses:
+        write_emails.writerow([email])
